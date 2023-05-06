882a976300844fb8d2d3d914ba542c5dc613940e
akash elhance <akashelhance@gmail.com>
2023-05-07 04:31:16 +0530

    Touchable Button Added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Text, StyleSheet , View, Button} from "react-native";
+import { Text, StyleSheet , View, Button, TouchableOpacity} from "react-native";
 
 const HomeScreen = () => {
   return <View>
     <Text style={styles.text}>HomeScreen l</Text>
-    <Button onPress={()=>console.log("Button Pressed")} title ="Go The Components Demo"/>
+    <Button 
+    onPress={()=>console.log("Button Pressed")} 
+    title ="Go The Components Demo"
+    />
+    <TouchableOpacity
+    onPress={()=>console.log("This is Second Button Clicked")}>
+      <Text>Go To List Components</Text>
+    </TouchableOpacity>
+
+    
+
+  
   </View>;
 };
 
