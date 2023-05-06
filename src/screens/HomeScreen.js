7c68625c8f49d090bd39a624711de8b601bc9877
akash elhance <akashelhance@gmail.com>
2023-05-07 04:40:59 +0530

    Changing Screen using Button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Text, StyleSheet , View, Button, TouchableOpacity} from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+  
   return <View>
     <Text style={styles.text}>HomeScreen l</Text>
     <Button 
-    onPress={()=>console.log("Button Pressed")} 
+    onPress={()=>props.navigation.navigate('Components')} 
     title ="Go The Components Demo"
     />
     <TouchableOpacity
-    onPress={()=>console.log("This is Second Button Clicked")}>
+     onPress={()=>props.navigation.navigate('List')} >
       <Text>Go To List Components</Text>
     </TouchableOpacity>
 
